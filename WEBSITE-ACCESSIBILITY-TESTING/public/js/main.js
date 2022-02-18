083421d19a54d24b813c870c7850cdb6bc5b7aa7
Oliver <oliver-shaddai.soto@atos.net>
2022-02-18 12:52:58 -0600

    Change the main js to charge the API data

---
+++ WEBSITE-ACCESSIBILITY-TESTING/public/js/main.js
@@ -0,0 +1,40 @@
+// Fetch accessibility
+const testAccessibility = async (e) => {
+    e.preventDefault();
+    
+
+    const url = document.querySelector('#url').value;
+    if (url === '') {
+        alert('please add url');
+    }else{
+        setLoading();
+        const response = await fetch('/api/test?url=${url}')
+
+        if (response.status !== 200) {
+            setLoading(false);
+            alert('something went wrong');
+        }else{
+            const {issues} = await response.json();
+            addIssuesToDOM(issues)
+            setLoading(false);
+        }
+    }
+}
+// Add issues to DOM
+const addIssuesToDOM = (issues) =>{
+    console.log(issues);
+}
+
+// Set loading state
+const setLoading = (isLoading = true) => {
+    const loader = document.querySelector('.loader')
+    if (isLoading) {
+        loader.style.display = 'block';
+    }else{
+        loader.style.display = 'none';
+    }
+}
+
+// Escape HTML
+
+document.querySelector("#form").addEventListener('submit', testAccessibility);
